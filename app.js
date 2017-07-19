c5a45dfb80bcbbed0e32af1d62d8c55399c7626b
Ubuntu 16.04 64-bit <ichwanharyosembodo96@gmail.com>
2017-07-18 22:01:05 -0700

    use socket for conversation

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
 
 var app = express();
+var server = http.Server(app);
+var io = socketio(server);
+
+server.listen(8080);
 
 app.get('/test',
     function (req, res) {
@@ -10,4 +16,9 @@ app.get('/test',
     }
 );
 
-app.listen(process.env.PORT || 8080);
+io.on('connection', function (socket) {
+    socket.emit('conversation', { text: 'hello world' });
+    socket.on('conversation', function (data) {
+        console.log(data);
+    });
+});
